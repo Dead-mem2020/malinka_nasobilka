e80b52144e77beb07de9535053214ca2a51a4389
Stanislav Roll <Deadmem2000@gmail.com>
2024-03-11 09:31:45 +0100

    funguje a počet odpovědí

--- malinka_nasobilka.py
+++ malinka_nasobilka.py
@@ -1,24 +1,35 @@
-import random #importujeme knihovnu s funkcema pro náhodné generování čísel
-
+#importujeme knihovnu s funkcema pro náhodné generování čísel
+import random 
 
 def nasobeni(a, b):
     vysledek = a * b
     return vysledek
 
-#v odpovedi chyba
 def vyhodnoceni(vysledek, porovnani):
     odpoved = False
+    #převedeme funkci porovnani do integeru
+    porovnani = int(porovnani)
     if vysledek == porovnani:
         print("No ty jsi ale šikulka, tebe snad vememe taky do Anglie")
         odpoved = True
     else:
-        print("Á, chybička se vbloudila")
+        print("Á, chybička se ti vbloudila")
     return odpoved
-   
-for i in range(9):
+
+
+spravne_odpovedi = 0
+spatne_odpovedi = 0
+
+for i in range(10):
     x = random.randint(1,10)
     y = random.randint(1,10)
-    #pravý alt + bn pro {}
+    #pravý alt + b pro {}
     porovnani = input(f"{x} * {y} = ")
     vysledek = nasobeni(y, x)
-    vyhodnoceni(vysledek, porovnani)
+
+    if vyhodnoceni(vysledek, porovnani):
+        spravne_odpovedi += 1
+    else:
+        spatne_odpovedi += 1
+
+    print(f"Správné odpovědi: {spravne_odpovedi}, špatné odpovědi: {spatne_odpovedi}")
